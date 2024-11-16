5b9efc6ba3c2c626b2c934fb99665abe1717140f
keshamr77 <kesha.mr77@gmail.com>
2024-11-16 16:05:41 +0700

    add npc to environment, hapus dulu web_app dll.

--- codes/entities.py
+++ codes/entities.py
@@ -1,13 +1,43 @@
-from pygame.sprite import Group
 from settings import *
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, pos, frames, groups, facing_direction):
         super().__init__(groups)
-        self.image = pygame.Surface((100, 100))
-        self.image.fill('red')
-        self.rect = self.image.get_rect(center=pos)
-		
+
+        # Graphics
+        self.frame_index, self.frames = 0, frames
+        self.facing_direction = facing_direction
+
+        # Movement
+        self.direction = vector()
+        self.speed = 250
+
+        # Sprite setup
+        self.image = self.frames[facing_direction][self.frame_index]
+        self.rect = self.image.get_frect(center=pos)
+    
+    def animate(self,dt) :
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][int(self.frame_index % len(self.frames[self.get_state()]))]
+
+    def get_state(self) :
+        moving = bool(self.direction)
+        if moving :
+            if self.direction.x != 0 :
+                self.facing_direction = 'right' if self.direction.x > 0 else 'left'
+            if self.direction.y != 0 :
+                self.facing_direction = 'down' if self.direction.y > 0 else 'up'        
+        return f'{self.facing_direction}{'' if moving else '_idle'}'
+        
+class Character(Entity) :
+    def __init__(self, pos, frames, groups, facing_direction):
+        super().__init__(pos, frames, groups, facing_direction)      
+
+class Player(Entity):
+    def __init__(self, pos, frames, groups, facing_direction):
+        super().__init__(pos, frames, groups, facing_direction)
+
+        # Movement
         self.direction = vector()
 
     def input(self):
@@ -21,11 +51,13 @@ class Player(pygame.sprite.Sprite):
             input_vector.x -= 1
         if keys[pygame.K_d]:
             input_vector.x += 1
-        self.direction = input_vector.normalize() if input_vector else input_vector 
+        self.direction = input_vector.normalize() if input_vector else input_vector
 
-    def move(self, dt):
+    def move(self, dt):  
         self.rect.center += self.direction * 250 * dt
-		
+    
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
--- codes/main.py
+++ codes/main.py
@@ -3,7 +3,7 @@ from pytmx.util_pygame import load_pygame
 from os.path import join, dirname, abspath
 
 from sprites import Sprite, AnimatedSprite
-from entities import Player
+from entities import Player, Character
 from groups import AllSprites
 
 from support import *
@@ -27,6 +27,7 @@ class Game:
         hospital_path =  join(base_path, "data", "maps", "hospital.tmx")
         water_path = join(base_path, "graphics", "tilesets", "water")
         coast_path = join(base_path, "graphics", "tilesets", "coast")
+        character_path = join(base_path, "graphics", "characters")
         self.tmx_maps = {
             'world': load_pygame(map_path), 
             'hospital' : load_pygame(hospital_path)
@@ -34,31 +35,53 @@ class Game:
         
         self.overworld_frames = {
             'water' : import_folder(water_path),
-            'coast' : coast_importer(24,12, coast_path)
+            'coast' : coast_importer(24,12, coast_path),
+            'characters' : all_character_import(character_path)
         }
 
     def setup(self, tmx_map, player_start_pos):
         # Terrain
         for layer in ['Terrain', 'Terrain Top'] :
             for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles() :
-                Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+                Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites, WORLD_LAYERS['bg'])
         
         
         # Entities
         for obj in tmx_map.get_layer_by_name('Entities') :
-            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-                self.player = Player((obj.x, obj.y), self.all_sprites)
+            if obj.name == 'Player' :
+                if obj.properties['pos'] == player_start_pos :
+                    self.player = Player(
+                        pos = (obj.x, obj.y), 
+                        frames = self.overworld_frames['characters']['player'],
+                        groups = self.all_sprites,
+                        facing_direction = obj.properties['direction'])
+            else :
+                Character(
+                    pos = (obj.x, obj.y), 
+                    frames = self.overworld_frames['characters'][obj.properties['graphic']],
+                    groups = self.all_sprites,
+                    facing_direction =  obj.properties['direction']
+                )
         
         # Objects
         for obj in tmx_map.get_layer_by_name('Objects') :
             Sprite((obj.x, obj.y), obj.image, self.all_sprites)
         
+        # Grass patches
+        for obj in tmx_map.get_layer_by_name('Monsters') :
+            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
         # Water 
         for obj in tmx_map.get_layer_by_name('Water') :
             for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE) : 
                 for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE) :
-                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
-                
+                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites, WORLD_LAYERS['water'])
+
+       # Coast
+        for obj in tmx_map.get_layer_by_name('Coast') :
+            terrain = obj.properties['terrain']
+            side = obj.properties['side'] 
+            AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites, WORLD_LAYERS['bg'])    
     def run(self) :
         while True :
             dt = self.clock.tick() / 1000
